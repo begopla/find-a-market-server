7c1b56da03721bf8e8da631d88ddbdcb0d327805
begopla <begonapr@gmail.com>
2022-07-19 23:47:54 +0400

    Initial commit

---
+++ models/User.model.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose")
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required:true,
+			unique: true -> Ideally, should be unique, but its up to you
+		},
+		email: String,
+		password: String,
+		profilePicture: String,
+		location: String,
+		bookmarkList:[
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Market"
+			},
+		],
+		usersFollowed: [
+			{
+				type: Schema.Types.ObjectId,
+				ref:'User'
+
+			},
+		],
+		typeOfCuisine:[{
+			type: String,
+			enum:["Asian Food", "French",  "Mediterranean", "Lebanese", "Turkish", "Indian", "Mexican", "Caribbean", "German", "Rusian", "American", "Others"]
+		}],
+		dietaryReq:[{
+			type: String,
+			enum:["Fast Food", "Vegetarian", "Vegan", "Low Carbs", "Healthy", "WholeFood", "Ecological", "Others"]
+		}],
+		eatingHabits:[{
+			type:String,
+			enum:["Cooking", "EatIn", "Delivery", "StreetFood", "Others"]
+		}],
+	},
+	{
+		// this second object adds extra properties: `createdAt` and `updatedAt`
+		timestamps: true,
+	}
+	
+)
+
+const User = model("User", userSchema)
+
+module.exports = User
