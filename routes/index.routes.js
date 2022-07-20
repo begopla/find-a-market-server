49a3c43dd037eabcaaf93ced990293e029c2d933
EugeBr <eugebordenave@gmail.com>
2022-07-20 17:21:14 +0200

    market routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
+const marketRoutes = require("./market.routes");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+router.use("/markets", marketRoutes);
 router.use("/auth", authRoutes);
 
 module.exports = router;
