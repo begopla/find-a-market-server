89be75a972b4bc7e117157fd4fd46cefb2e963f6
begopla <begonapr@gmail.com>
2022-07-21 11:02:55 +0400

    added users preferences

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -34,6 +34,30 @@ router.get("/", isAuthenticated, (req, res, next)=>{
     
 });
 
+// C(R)UD -- Read and return followed users
+
+router.get("/followed",isAuthenticated, async (req, res, next) =>{
+
+    try {
+        userId= req.payload._id;
+        const user = await User.findById(userId).populate("usersFollowed");
+        const followedUsers = user.usersFollowed;
+        res.status(200).json({
+            savedList: followedUsers,
+        });
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.get("")
+
+router.get("/", isAuthenticated, (req, res, next)=>{
+    res.status(200).json(req.payload);
+    
+});
+
 // CR(U)D -- Update user object adding image string
 router.put('/',isAuthenticated, async(req, res, next) =>{
     try {
@@ -81,7 +105,37 @@ router.post('/upload', isAuthenticated, async (req,res,next) =>{
     }
   });
 
-  router.delete("/:")
+ 
+// CR(U)D -- Update user object adding user preferences 
+router.put('/user-info',isAuthenticated, async(req, res, next) =>{
+    try {
+       const {typeOfCuisine, dietaryReq} =req.body;
+       const user = await User.findByIdAndUpdate(
+        req.payload._id,
+        {
+            typeOfCuisine,
+            dietaryReq,
+        },
+        { new: true}
+       );
+       const authToken = jwt.sign(user.toObject(), process.env.TOKEN_SECRET,{
+        algorithm: "HS256",
+        expiresIn: "2d",
+       });
+       console.log(typeOfCuisine, dietaryReq)
+       res.status(200).json({
+          
+        message: 'File successfully uploaded',
+        user: req.payload,
+        token: authToken
+
+      });
+
+    } catch (error) {
+        next(error);
+    }
+})
+
 
 
 module.exports = router;
